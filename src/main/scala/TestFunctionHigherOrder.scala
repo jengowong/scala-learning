d48aff847b71ea610ae0dfcd438bd4e95377a369
wangzhenguo02 <wangzhenguo02@meituan.com>
2016-06-23 20:06:54 +0800

    test

---
+++ src/main/scala/TestFunctionHigherOrder.scala
@@ -0,0 +1,15 @@
+/**
+ * Scala 高阶函数
+ */
+object TestFunctionHigherOrder {
+
+    def main(args: Array[String]) {
+        println(apply(layout, 10))
+    }
+
+    //函数 f 和 值 v 作为参数，而函数 f 又调用了参数 v
+    def apply(f: Int => String, v: Int) = f(v)
+
+    def layout[A](x: A) = "[" + x.toString() + "]"
+
+}
